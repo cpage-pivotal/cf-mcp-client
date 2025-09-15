5fcfbd780a62c4c4fc0f5f7bc196fa6f2a0cc797
cpage-pivotal <corby.page@broadcom.com>
2025-09-14 19:19:16 -0500

    v1.6.0

--- src/main/java/org/tanzu/mcpclient/CfMcpClientApplication.java
+++ src/main/java/org/tanzu/mcpclient/CfMcpClientApplication.java
@@ -1,7 +1,5 @@
 package org.tanzu.mcpclient;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +9,5 @@ public class CfMcpClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CfMcpClientApplication.class, args);
 	}
-
-
-    private static final Logger logger = LoggerFactory.getLogger(CfMcpClientApplication.class);
 
 }
